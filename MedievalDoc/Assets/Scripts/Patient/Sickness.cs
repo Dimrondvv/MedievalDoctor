a4ecb3e27c975af29d23c263d0ad2755bfae2615
SoVerrr <patryk.rokiczan@gmail.com>
2023-12-24 18:52:06 +0100

    ????????????

--- MedievalDoc/Assets/Scripts/Patient/Sickness.cs
+++ MedievalDoc/Assets/Scripts/Patient/Sickness.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "SicknessScriptable", menuName = "ScriptableObjects", order = 1)]
+[CreateAssetMenu(fileName = "SicknessScriptable", menuName = "ScriptableObjects/Sickness", order = 1)]
 public class SicknessScriptableObject : ScriptableObject
 {
     public List<GameObject> toolsRequired;
