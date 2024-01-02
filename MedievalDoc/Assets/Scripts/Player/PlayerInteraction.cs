838c0928e6315a30b203adcb469fae1a4ca69471
SoVerrr <patryk.rokiczan@gmail.com>
2024-01-02 19:02:40 +0100

    Revert "Merge branch 'main' into Blueprint-snap"
    This reverts commit 50aaefd35e4644b5ac9a57a87b4f526f86b88983, reversing
    changes made to 0cf67dc3dd8304486689f675fb60108a8e9cd541.

--- MedievalDoc/Assets/Scripts/Player/PlayerInteraction.cs
+++ MedievalDoc/Assets/Scripts/Player/PlayerInteraction.cs
@@ -9,7 +9,7 @@ public class PlayerInteraction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) || Input.GetButtonDown("Interact"))
+        if (Input.GetKeyDown(KeyCode.E))
         {
             RaycastHit hit;
             if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, interactionRange))
