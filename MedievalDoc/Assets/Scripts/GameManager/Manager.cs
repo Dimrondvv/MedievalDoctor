9d698a9d8f25e7dc6e2cda7a844a857b5e364859
JUAN PABLO <111735636+shary262@users.noreply.github.com>
2024-01-05 18:01:15 +0100

    added gamemanager and deathcounter

--- MedievalDoc/Assets/Scripts/GameManager/Manager.cs
+++ MedievalDoc/Assets/Scripts/GameManager/Manager.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class gameInfo : MonoBehaviour
+public class Manager : MonoBehaviour
 {
+    [SerializeField] int maxDeaths;
+    public int deathCounter;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,8 @@ public class gameInfo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(deathCounter >= maxDeaths){
+	    Debug.Log("MAX DEATHS");
+	}
     }
 }
