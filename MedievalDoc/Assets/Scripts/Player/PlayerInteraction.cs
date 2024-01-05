331c4550d1ce6194ad19adfa850e9db682f26fd0
SoVerrr <patryk.rokiczan@gmail.com>
2024-01-05 20:49:02 +0100

    Fix interaction

--- MedievalDoc/Assets/Scripts/Player/PlayerInteraction.cs
+++ MedievalDoc/Assets/Scripts/Player/PlayerInteraction.cs
@@ -12,8 +12,10 @@ public class PlayerInteraction : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E))
         {
             RaycastHit hit;
+
             if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, interactionRange))
             {
+
                 IInteractable interactable = hit.transform.gameObject.GetComponent<IInteractable>();
                 if (interactable != null)
                     interactable.Interact();
