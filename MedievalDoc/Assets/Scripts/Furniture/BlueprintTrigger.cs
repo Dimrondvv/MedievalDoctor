d8108abe99b850da7a21fa2ce47cd102b903c2be
Dimrondvv <83478759+Dimrondvv@users.noreply.github.com>
2024-01-12 20:12:58 +0100

    ToFix multipe mesh rederer

--- MedievalDoc/Assets/Scripts/Furniture/BlueprintTrigger.cs
+++ MedievalDoc/Assets/Scripts/Furniture/BlueprintTrigger.cs
@@ -10,40 +10,91 @@ public class BlueprintTrigger : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         isPlacable = false;
-        ChangeBlueprintToBlue(this.gameObject);
+        ChangeBlueprintToRed(this.gameObject);
     }
 
     private void OnTriggerExit(Collider other)
     {
         isPlacable = true;
-        ChangeBlueprintToRed(this.gameObject);
+        ChangeBlueprintToBlue(this.gameObject);
     }
 
     public void ChangeBlueprintToBlue(GameObject obj) {
         if (obj.transform.childCount > 0) {// Check if object contains chilndren
-            var mats = obj.transform.GetChild(0).gameObject.GetComponent<Renderer>().materials; // Create list of materials to change
+            Transform firstChild = obj.transform.GetChild(0);
+  
+            if (firstChild.childCount > 0) // Check if children has any childrens
+            {
+                Transform firstChildOfaChild = firstChild.transform.GetChild(0);
 
-            for (int i = 0; i < mats.Length; i++) {
-                mats[i] = blueprintRed; // Change all material to chosen
-            }
+                var mats = firstChildOfaChild.gameObject.GetComponent<Renderer>().materials; // Create list of materials to change
 
-            obj.transform.GetChild(0).gameObject.GetComponent<Renderer>().materials = mats; // Assign materials to gameObject
+                for (int i = 0; i < mats.Length; i++)
+                {
+                    mats[i] = blueprintBlue; // Change all material to chosen
+                }
+
+                obj.transform.GetChild(0).transform.GetChild(0).gameObject.GetComponent<Renderer>().materials = mats; // Assign materials to child gameObject
+            }else
+            {
+                var mats = obj.transform.GetChild(0).gameObject.GetComponent<Renderer>().materials; // Create list of materials to change
+
+                for (int i = 0; i < mats.Length; i++)
+                {
+                    mats[i] = blueprintBlue; // Change all material to chosen
+                }
+
+                obj.transform.GetChild(0).gameObject.GetComponent<Renderer>().materials = mats; // Assign materials to gameObject
+            } 
         } else {
-            GetComponent<Renderer>().material = blueprintRed;
+            GetComponent<Renderer>().material = blueprintBlue;
         }
     }
 
     private void ChangeBlueprintToRed(GameObject obj) {
-        if (obj.transform.childCount > 0) {// Check if object contains chilndren
-            var mats = obj.transform.GetChild(0).gameObject.GetComponent<Renderer>().materials; // Create list of materials to change
+        //if (obj.transform.childCount > 0) {// Check if object contains chilndren
+        //    var mats = obj.transform.GetChild(0).gameObject.GetComponent<Renderer>().materials; // Create list of materials to change
 
-            for (int i = 0; i < mats.Length; i++) {
-                mats[i] = blueprintBlue; // Change all material to chosen
+        //    for (int i = 0; i < mats.Length; i++) {
+        //        mats[i] = blueprintBlue; // Change all material to chosen
+        //    }
+
+        //    obj.transform.GetChild(0).gameObject.GetComponent<Renderer>().materials = mats; // Assign materials to gameObject
+        //} else {
+        //    GetComponent<Renderer>().material = blueprintBlue;
+        //}
+        if (obj.transform.childCount > 0)
+        {// Check if object contains chilndren
+            Transform firstChild = obj.transform.GetChild(0);
+
+            if (firstChild.childCount > 0) // Check if children has any childrens
+            {
+                Transform firstChildOfaChild = firstChild.transform.GetChild(0);
+
+                var mats = firstChildOfaChild.gameObject.GetComponent<Renderer>().materials; // Create list of materials to change
+
+                for (int i = 0; i < mats.Length; i++)
+                {
+                    mats[i] = blueprintRed; // Change all material to chosen
+                }
+
+                obj.transform.GetChild(0).transform.GetChild(0).gameObject.GetComponent<Renderer>().materials = mats; // Assign materials to child gameObject
             }
+            else
+            {
+                var mats = obj.transform.GetChild(0).gameObject.GetComponent<Renderer>().materials; // Create list of materials to change
 
-            obj.transform.GetChild(0).gameObject.GetComponent<Renderer>().materials = mats; // Assign materials to gameObject
-        } else {
-            GetComponent<Renderer>().material = blueprintBlue;
+                for (int i = 0; i < mats.Length; i++)
+                {
+                    mats[i] = blueprintRed; // Change all material to chosen
+                }
+
+                obj.transform.GetChild(0).gameObject.GetComponent<Renderer>().materials = mats; // Assign materials to gameObject
+            }
+        }
+        else
+        {
+            GetComponent<Renderer>().material = blueprintRed;
         }
     }
 }
