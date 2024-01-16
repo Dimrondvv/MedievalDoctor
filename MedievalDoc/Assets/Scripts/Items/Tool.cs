c7e732d9e6624d88b332c509856b50ec60e13e92
SoVerrr <patryk.rokiczan@gmail.com>
2024-01-16 21:47:36 +0100

    Fix random issues

--- MedievalDoc/Assets/Scripts/Items/Tool.cs
+++ MedievalDoc/Assets/Scripts/Items/Tool.cs
@@ -29,12 +29,12 @@ public class Tool : MonoBehaviour
         if (tool != this.gameObject || !symptomRemoved.isValid)
             return;
 
-        patient.sickness.RemoveSymptom(symptomAdded.symptom);
+        patient.sickness.RemoveSymptom(symptomRemoved.symptom);
 
         if(patient.sickness.CheckIfCured())
             PatientEventManager.Instance.OnCureDisease.Invoke(patient);
         else
-            PatientEventManager.Instance.OnRemoveSymptom.Invoke(tool, patient);
+            PatientEventManager.Instance.OnRemoveSymptom.Invoke(symptomRemoved.symptom, patient);
 
     }
     private void CheckSymptom(GameObject tool, Patient patient)
