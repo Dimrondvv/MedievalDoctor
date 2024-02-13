68871c5a281a46804456e0c7f48419fb14b081f3
JUAN PABLO <111735636+shary262@users.noreply.github.com>
2024-02-13 19:31:57 +0100

    prawie skończony system updateów
    1. dodać wyskakujące okienko w którym gracz wybierze upgrade
    2. dodawanie instancji toola
    3. na podstawie listy possessedTools zrobić listę możliwych do wyleczenia symptomów

--- MedievalDoc/Assets/Scripts/Managers/UpgradeManager.cs
+++ MedievalDoc/Assets/Scripts/Managers/UpgradeManager.cs
@@ -5,39 +5,49 @@ using System.Linq;
 
 public class UpgradeManager : MonoBehaviour
 {
-    private List<Tool> possessedTools; // Tools you have
-    private List<Symptom> curableSymptoms; // Symptoms that are curable with your current equipment
+    [SerializeField] private List<Tool> possessedTools; // Tools you have
+    [SerializeField] private List<Tool> fullToolList;
+    [SerializeField] private int numberOfUpgradesOnUpgrade;
+    //private List<Symptom> curableSymptoms; // Symptoms that are curable with your current equipment
 
-    private void MakeMeChoose(List<Tool> fullToolList)
+    private List<string> fullToolListNames;
+    private List<string> possessedToolsNames;
+
+    private void MakeToolForMe(string toolName)
     {
-        List<Tool> toolPool = fullToolList.Except(possessedTools).ToList(); // List of not earned yet items
-        // Debug.Log(toolPool);
-        // wypluæ np 3 mo¿liwe toolsy do wyboru -> chyba okienko z wyborem po prostu
-        // po wybraniu dodaæ toola do listy posiadanych i na jego podstawie zaktualizowaæ curable symptomy 
-        // po tym spawn toola? i guess
+        //mo¿e coœ takiego?
+        //newtool = Instantiate(GameObject.Find(toolName));
+        //i po tym do possessedToolsNames dodaæ newtool'a
+        //
+    }
+    private void MakeMeChoose(List<string> toolList)
+    {
+        List<string> toolsToBeChoosed;
+        //List<Tool> toolPool = fullToolListNames.Except(possessedToolsNames).ToList(); // List of not earned yet items
+        if (toolList.Count < numberOfUpgradesOnUpgrade)
+        {
+            toolsToBeChoosed = toolList.OrderBy(x => UnityEngine.Random.value).Take(numberOfUpgradesOnUpgrade).ToList();
+        } else
+        {
+            toolsToBeChoosed = toolList.OrderBy(x => UnityEngine.Random.value).Take(toolList.Count).ToList();
+        }
+
+        // Tu se gracz wybiera np pierwszy element :clueless:
+        MakeToolForMe(toolsToBeChoosed[0]);
     }
 
 
-    private List<Tool> tempListOfTools;
     private void Start() // i made it up UwU :3 :D :) ;p
     {
-        /*possessedTools[0].name = "banan";     //no to tak to jest do pizdy, trzeba zrobiæ <- tu debilu zacznij
-        possessedTools[1].name = "gruszka";     //klase tymczasow¹ nawet z samym stringim i doprowadziæ to do dzia³ania
-        possessedTools[2].name = "brzoskwinia"; //jak zacznie dzia³aæ to poprzerabiaæ wszystkie toolsy tak ¿eby
-        tempListOfTools[0].name = "drzewo";     //wpisywa³y siê na listê
-        tempListOfTools[1].name = "banan";
-        tempListOfTools[2].name = "pies";
-        tempListOfTools[3].name = "gruszka";
-        tempListOfTools[4].name = "hihiahah";
-        tempListOfTools[5].name = "brzoskwinia";*/
-
+        fullToolListNames = fullToolList.Select(o => o.name).ToList();
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.U)) // https://youtu.be/ujUKcNZ-rK4?si=9zcX_2YXuFKOEOUh
         {
-            MakeMeChoose(tempListOfTools);
+            possessedToolsNames = possessedTools.Select(o => o.name).ToList(); // to do eventu z upgradem wywaliæ
+            MakeMeChoose(fullToolListNames.Except(possessedToolsNames).ToList());
         }
     }
 }
