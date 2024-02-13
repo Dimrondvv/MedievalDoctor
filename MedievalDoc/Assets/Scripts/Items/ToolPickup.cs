bfbb072a49b2a51e09e981f270d29f5f283174fa
Dimrondvv <dominik.glowinski2@gmail.com>
2024-02-13 01:00:05 +0100

    Adjust code in itemPickup

--- MedievalDoc/Assets/Scripts/Items/ToolPickup.cs
+++ MedievalDoc/Assets/Scripts/Items/ToolPickup.cs
@@ -7,9 +7,9 @@ public class ToolPickup : MonoBehaviour
     //private GameObject player;
     private GameObject finger;
     private Transform toolPickupPoint;
-    private PickupController playerController;
+    private PickupController pickupController;
 
-    private GameObject pickedToolObject;
+
 
     void Start()
     {
@@ -19,52 +19,52 @@ public class ToolPickup : MonoBehaviour
 
         toolPickupPoint = PlayerManager.Instance.PickupController.GetToolPickupPoint();
         finger = PlayerManager.Instance.PickupController.GetFingerObject();
-        playerController = PlayerManager.Instance.PickupController.GetPickupController();
+        pickupController = PlayerManager.Instance.PickupController.GetPickupController();
     }
 
-    private void Update()
-    {
-        //RotatePickedObject(pickedToolObject, toolPickupPoint);
-    }
 
     private void PickupTool(GameObject pickedTool, Transform objectPoint) {
+        if (objectPoint == null) {
+            return;
+        }
+
+        ItemLayDownPoint LayDownPoint = objectPoint.GetComponentInChildren<ItemLayDownPoint>();
+
         if (pickedTool == this.gameObject) {
             if (objectPoint != null) {
-                objectPoint.GetComponentInChildren<ItemLayDownPoint>().checkIfOccupied = false;
+                LayDownPoint.checkIfOccupied = false;
             }
 
-            pickedToolObject = pickedTool;
+            pickupController.SetPickedItem(pickedTool);
 
-            playerController.SetPickedItem(pickedTool);
-            //pickedTool.GetComponent<Collider>().enabled = false;
             pickedTool.transform.position = toolPickupPoint.position;
-            
             pickedTool.transform.SetParent(finger.transform);
             pickedTool.GetComponent<MeshCollider>().enabled = false;
-            //pickedTool.transform.eulerAngles = finger.transform.eulerAngles + new Vector3(0f, 90f, 0f);
-            //  var lastChild = player.transform.childCount - 1;
-
-            //player.transform.GetChild(lastChild).localEulerAngles = new Vector3(0, 0, 0);
         }
     }
 
     private void PutdownTool(PickupController pickedTool, Transform pickupPoint) {
-        if (pickedTool.PickedItem != null && playerController.PickedItem.GetComponent<Tool>() != null && pickupPoint && pickupPoint.GetComponentInChildren<ItemLayDownPoint>() && !pickupPoint.GetComponentInChildren<ItemLayDownPoint>().checkIfOccupied) {
-            GameObject putDownTool = pickedTool.PickedItem;
-            pickupPoint.GetComponentInChildren<ItemLayDownPoint>().checkIfOccupied = true;
-            putDownTool.GetComponent<MeshCollider>().enabled = true;
-            var offset = putDownTool.GetComponent<MeshCollider>().bounds.size;
-            putDownTool.transform.position = pickupPoint.GetComponentInChildren<ItemLayDownPoint>().transform.position + new Vector3(0, offset.y/2, 0);
-            putDownTool.transform.rotation = pickupPoint.GetComponentInChildren<ItemLayDownPoint>().transform.rotation;
-            putDownTool.transform.SetParent(pickupPoint);
-            playerController.SetPickedItem(null);
+        if (pickedTool.PickedItem == null || pickupPoint == null) {
+            return;
         }
-    }
 
-    private void RotatePickedObject(GameObject pickedTool, Transform objectPoint){
-        if (pickedTool != null && objectPoint != null)
-        {
-            pickedTool.transform.eulerAngles = objectPoint.eulerAngles + new Vector3(0f, 90f, 0);
+
+        Tool tool = pickedTool.PickedItem.GetComponent<Tool>();
+        ItemLayDownPoint LayDownPoint = pickupPoint.GetComponentInChildren<ItemLayDownPoint>();
+
+        if (tool && LayDownPoint && !LayDownPoint.checkIfOccupied) {
+            GameObject putDownTool = pickedTool.PickedItem;
+            MeshCollider pickedToolMeshCollider = putDownTool.GetComponent<MeshCollider>();
+     
+            LayDownPoint.checkIfOccupied = true;
+            pickedToolMeshCollider.enabled = true;
+
+            Vector3 offset = pickedToolMeshCollider.bounds.size;
+
+            putDownTool.transform.position = LayDownPoint.transform.position + new Vector3(0, offset.y/2, 0);
+            putDownTool.transform.rotation = LayDownPoint.transform.rotation;
+            putDownTool.transform.SetParent(pickupPoint);
+            pickedTool.SetPickedItem(null);
         }
     }
 }
