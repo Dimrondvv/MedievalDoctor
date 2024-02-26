a3be143b9b4a7b0d28614592b2ba906ded7d3411
JUAN PABLO <111735636+shary262@users.noreply.github.com>
2024-02-27 00:28:11 +0100

    nie mam pojęcia co robię :)

--- MedievalDoc/Assets/Scripts/Managers/UpgradeManager.cs
+++ MedievalDoc/Assets/Scripts/Managers/UpgradeManager.cs
@@ -2,70 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.UI;
 
 public class UpgradeManager : MonoBehaviour
 {
     [SerializeField] private List<Tool> possessedTools; // Tools you have
     [SerializeField] private List<Tool> fullToolList;
     [SerializeField] private UpgradeWindow upgradeWindow;
-    [SerializeField] private List<ItemLayDownPoint> counterList;
+    [SerializeField] private List<ItemLayDownPoint> leydownPoints;
+    [SerializeField] private Sprite potka; // temp potka dla sprajtów których nie ma
     //private List<Symptom> curableSymptoms; // Symptoms that are curable with your current equipment
     private List<string> fullToolListNames;
     private List<string> possessedToolsNames;
-
     public void MakeToolForMe(string toolName)
     {
-        
         for (int i = 0; i < fullToolList.Count; i++)
         {
             if (toolName == fullToolList[i].name)
             {
-                for( int j = 0; j < counterList.Count; j++)
-                {
-                    if (!counterList[j].checkIfOccupied)
-                    {
-                        Instantiate(fullToolList[i], counterList[j].transform.position, Quaternion.identity);
-                        possessedTools.Add(fullToolList[i]);
-                        return;
-                    }
-                }
-                
+                Debug.Log("co jest");
+                Instantiate(fullToolList[i], new Vector3(11.38f, 1f, 21.75f), Quaternion.identity);
+                possessedTools.Add(fullToolList[i]);
+                return;
             }
         }
-        //ListOfSymptoms(); // tu aktualizuj liste curable symptomów
     }
 
-    /*private void ListOfSymptoms()
-    {
-        for(int i = 0; i < possessedTools.Count; i++)
-        {
-            for(int j  = 0; j < possessedTools[i].SymptomsRemoved.Count; j++)
-            {
-                Debug.Log(possessedTools[i].SymptomsRemoved[j]);
-                curableSymptoms.Add(possessedTools[i].SymptomsRemoved[j]);
-                if (!curableSymptoms.Contains(possessedTools[i].SymptomsRemoved[j]))
-                {
-                    
-                    Debug.Log(curableSymptoms.Count);
-                }
-            }
-        }
-        //curableSymptoms = curableSymptoms.Distinct().ToList();
-    }*/
-
-    private void MakeMeChoose(List<string> toolList)
-    {
-        List<string> toolsToBeChoosed;
-        if (toolList.Count >= 3)
-        {
-            toolsToBeChoosed = toolList.OrderBy(x => UnityEngine.Random.value).Take(3).ToList();
-        } else
-        {
-            toolsToBeChoosed = toolList.OrderBy(x => UnityEngine.Random.value).Take(toolList.Count).ToList();
-        }
-        Time.timeScale = 0f;
-        upgradeWindow.PlayerChoice(toolsToBeChoosed, fullToolList);
-    }
 
 
     private void Start() // i made it up UwU :3 :D :) ;p
@@ -73,10 +35,9 @@ public class UpgradeManager : MonoBehaviour
         fullToolListNames = fullToolList.Select(o => o.name).ToList();
     }
 
-    private void UpgradeMyClinic() // https://youtu.be/ujUKcNZ-rK4?si=9zcX_2YXuFKOEOUh
+    private void ToolUpgrade() // https://youtu.be/ujUKcNZ-rK4?si=9zcX_2YXuFKOEOUh
     {
         possessedToolsNames = possessedTools.Select(o => o.name).ToList();
-
         for (int i = 0; i < possessedToolsNames.Count; i++)
         {
             for (int j = 0; j < fullToolListNames.Count; j++)
@@ -87,13 +48,40 @@ public class UpgradeManager : MonoBehaviour
                 }
             }
         }
-        MakeMeChoose(fullToolListNames);
+        List<string> toolsToBeChoosed;
+        if (fullToolListNames.Count >= 3)
+        {
+            toolsToBeChoosed = fullToolListNames.OrderBy(x => UnityEngine.Random.value).Take(3).ToList();
+        }
+        else
+        {
+            toolsToBeChoosed = fullToolListNames.OrderBy(x => UnityEngine.Random.value).Take(fullToolListNames.Count).ToList();
+        }
+        List<Sprite> imageList = new List<Sprite>();
+        for (int i = 0; i < toolsToBeChoosed.Count; i++)
+        {
+            for (int j = 0; j < fullToolList.Count; j++)
+            {
+                if (toolsToBeChoosed[i] == fullToolList[j].name)
+                {
+                    imageList.Add(fullToolList[j].ItemIcon);
+                }
+            }
+        }
+        upgradeWindow.PlayerChoice(toolsToBeChoosed, imageList, "tool");
     }
+
+    private void CraftingUpgrade()
+    {
+
+    }
+
     private void Update() // to do eventu z upgradem wywaliæ
     {
         if (Input.GetKeyDown(KeyCode.U))
         {
-            UpgradeMyClinic();
+            //ToolUpgrade(); // bo¿e coœ polskê odpalaj to tylko raz bo olaboga
+            CraftingUpgrade();
         }
     }
 }
--- MedievalDoc/Assets/Scripts/UI/UpgradeWindow.cs
+++ MedievalDoc/Assets/Scripts/UI/UpgradeWindow.cs
@@ -12,60 +12,90 @@ public class UpgradeWindow : MonoBehaviour
     [SerializeField] private List<Image> buttonSprites;
     [SerializeField] private UpgradeManager upgradeManager;
     private List<Tool> availableToolList;
+    private string typUpgradu;
 
-    void Start()
+
+    public void PlayerChoice(List<string> toolList, List<Sprite> imageList, string typ)
     {
+        typUpgradu = typ;
         buttons[0].onClick.AddListener(Click1);
         buttons[1].onClick.AddListener(Click2);
         buttons[2].onClick.AddListener(Click3);
-    }
-
-    public void PlayerChoice(List<string> toolList, List<Tool> fullToolList)
-    {
+        Time.timeScale = 0f;
         updateUI.SetActive(true);
-        availableToolList = fullToolList; // czy to jest tu potrzebne? :suscat:
-        
-        for( int i = 0; i < buttons.Count; i++ )
+        for ( int i = 0; i < buttons.Count; i++ )
         {
             buttons[i].gameObject.SetActive(true);
             buttonText[i].text = toolList[i];
             buttonSprites[i].gameObject.SetActive(true);
-            for( int j = 0; j < fullToolList.Count; j++)
-            {
-                if (toolList[i] == fullToolList[j].name)
-                {
-                    buttonSprites[i].sprite = fullToolList[j].ItemIcon;
-                }
-            }
+            buttonSprites[i].sprite = imageList[i];
         }
+
     }
     
 
     private void Click1()
     {
-        upgradeManager.MakeToolForMe(buttonText[0].text);
+        if (typUpgradu == "tool")
+        {
+            upgradeManager.MakeToolForMe(buttonText[0].text);
+        } else if(typUpgradu == "crafting")
+        {
+            // tu zrob crafting
+        }
+        else
+        {
+            //tu zrob item inny
+        }
+        
+        
         DisableDilemma();
     }
     private void Click2()
     {
-        upgradeManager.MakeToolForMe(buttonText[1].text);
+        if (typUpgradu == "tool")
+        {
+            upgradeManager.MakeToolForMe(buttonText[1].text);
+        }
+        else if (typUpgradu == "crafting")
+        {
+            // tu zrob crafting
+        }
+        else
+        {
+            //tu zrob item inny
+        }
         DisableDilemma();
     }
     private void Click3()
     {
-        upgradeManager.MakeToolForMe(buttonText[2].text);
+        if (typUpgradu == "tool")
+        {
+            upgradeManager.MakeToolForMe(buttonText[2].text);
+        }
+        else if (typUpgradu == "crafting")
+        {
+            // tu zrob crafting
+        }
+        else
+        {
+            //tu zrob item inny
+        }
         DisableDilemma();
     }
 
     private void DisableDilemma()
     {
+        Time.timeScale = 1f;
         for (int i = 0; i < buttons.Count; i++)
         {
             buttons[i].gameObject.SetActive(false);
             buttonSprites[i].gameObject.SetActive(false);
+            buttonText[i].text = null;
+            buttonSprites[i].sprite = null;
         }
         updateUI.SetActive(false);
-        Time.timeScale = 1f;
+        
     }
 
 
