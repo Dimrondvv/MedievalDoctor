8f39cd54630bb88e17a0f898ca92c4b4f29c0f2d
xMikuuu <m.polatynski9@gmail.com>
2024-02-28 11:17:20 +0100

    fixed build error

--- MedievalDoc/Assets/Scripts/Patrons/Quest.cs
+++ MedievalDoc/Assets/Scripts/Patrons/Quest.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "QuestScriptableObject", menuName = "ScriptableObjects/Quest", order = 4)]
