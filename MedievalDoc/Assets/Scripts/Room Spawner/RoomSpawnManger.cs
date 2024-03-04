5d8800006e9dec126adad7c7d1cc9c3c9d4062af
Dimrondvv <83478759+Dimrondvv@users.noreply.github.com>
2024-03-04 19:58:12 +0100

    Added random room spawning

--- MedievalDoc/Assets/Scripts/Room Spawner/RoomSpawnManger.cs
+++ MedievalDoc/Assets/Scripts/Room Spawner/RoomSpawnManger.cs
@@ -6,15 +6,67 @@ public class RoomSpawnManger : MonoBehaviour
 {
     [SerializeField]
     private List<GameObject> pokoje = new List<GameObject>();
+
+    private GameObject prevRoom;
+    private int prevDir = 69;
     // Start is called before the first frame update
     void Start()
     {
+        prevRoom = Instantiate(pokoje[0], new Vector3(0, 0, 0), Quaternion.identity);
         
+        InvokeRepeating("spawnRoom", 1f, 5f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    private void spawnInitialRoom()
+    {
+        //prevRoom = Instantiate(pokoje[0], new Vector3(0,0,0), Quaternion.identity);
+    }
+
+    private void spawnRoom()
+    {
+        var randomRoom = Random.Range(0, pokoje.Count);
+        var direction = Random.Range(0, 3);
+        
+        Vector3 position = prevRoom.transform.position;
+        Debug.Log("Dir: " + direction + " Prev: " + prevDir);
+        if (direction == prevDir)
+        {
+            direction = Random.Range(0, 3);
+        }
+        else
+        {
+            
+            switch (direction)
+            {
+                case 0:
+                    prevDir = 2;
+                    position.x -= prevRoom.GetComponent<Collider>().bounds.size.x;
+                    break;
+
+                case 1:
+                    prevDir = 3;
+                    position.z += prevRoom.GetComponent<Collider>().bounds.size.z;
+                    break;
+
+                case 2:
+                    prevDir = 0;
+                    position.x += prevRoom.GetComponent<Collider>().bounds.size.x;
+                    break;
+
+                case 3:
+                    prevDir = 1;
+                    position.z -= prevRoom.GetComponent<Collider>().bounds.size.z;
+                    break;
+            }
+            //Debug.Log(position);
+            prevRoom = Instantiate(pokoje[randomRoom], new Vector3(position.x, 0f, position.z), Quaternion.identity);
+        }
         
     }
 }
