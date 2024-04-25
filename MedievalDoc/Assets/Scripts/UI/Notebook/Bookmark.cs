eaf05d247a75700bb0888f144ffa7d0d2775cd80
SoVerrr <82769474+SoVerrr@users.noreply.github.com>
2024-04-25 20:44:24 +0200

    Notebook scroll view in bookmark

---
+++ MedievalDoc/Assets/Scripts/UI/Notebook/Bookmark.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class Bookmark : MonoBehaviour
+{
+    [SerializeField] string bookmarkName;
+    [SerializeField] TextMeshProUGUI nameTextField;
+    [SerializeField] GridLayoutGroup iconLayout;
+}
--- MedievalDoc/Assets/Scripts/UI/Notebook/PatientNotebook.cs
+++ MedievalDoc/Assets/Scripts/UI/Notebook/PatientNotebook.cs
@@ -5,20 +5,10 @@ using TMPro;
 using UnityEngine.UI;
 public class PatientNotebook : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI symptomsText;
-    [SerializeField] private TextMeshProUGUI historyText;
-    [SerializeField] private Image patientColorField;
-    [SerializeField] private TextMeshProUGUI patientName;
     [SerializeField] private List<Button> bookmarks;
     private PlayerInputActions playerInputActions;
-    private Patient patient;
-    private int currentPatientIndex = 0;
     private int currentBookmarkIndex = 0;
-    public Patient Patient
-    {
-        get { return patient; }
-        set { patient = value; }
-    }
+    
 
     private void Awake()
     {
@@ -28,42 +18,7 @@ public class PatientNotebook : MonoBehaviour
 
     }
 
-    public void SetNotebookSymptoms(Patient patient)
-    {
-        if (patient == null)
-            return;
-        symptomsText.text = "";
-        foreach (var symptom in patient.DiscoveredSymptoms.Values)
-        {
-            symptomsText.text += $"-{symptom} \n";
-        }
-        if(symptomsText.text == "")
-        {
-            symptomsText.text = "No symptoms";
-        }
-    }
-    public void SetNotebookHistory(Patient patient)
-    {
-        if (patient == null)
-            return;
-        historyText.text = patient.patientStory;
-        //TODO: SET HISTORIA
-    }
-    public void SetPatientColor(Patient patient)
-    {
-        if (patient == null)
-            return;
-        patientColorField.color = patient.GetComponentInChildren<Renderer>().material.color;
-    }
-    public void SetPatientName(Patient patient)
-    {
-        if (patient == null)
-        {
-            patientName.text = "No patients";
-            return;
-        }
-        patientName.text = patient.PatientName;
-    }
+    
 
     public void ChangeBookMark(UnityEngine.InputSystem.InputAction.CallbackContext callback)
     {
