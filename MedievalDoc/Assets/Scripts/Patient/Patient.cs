f35ab30dc5644f652c9b51701e7873508eb6347b
xMikuuu <83476709+xMikuuu@users.noreply.github.com>
2024-05-31 17:23:55 +0200

    Update Patient.cs

--- MedievalDoc/Assets/Scripts/Patient/Patient.cs
+++ MedievalDoc/Assets/Scripts/Patient/Patient.cs
@@ -74,6 +74,7 @@ public class Patient : MonoBehaviour
 
     public void Death()
     {
+        OnPatientDeath.Invoke(this);
         App.Instance.GameplayCore.GameManager.CheckDeathCounter();
         App.Instance.GameplayCore.GameManager.deathCounter+=1;
         PickupController.OnInteract.RemoveListener(InteractWithPatient);
