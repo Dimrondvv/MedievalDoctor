9d304eef0bb114d9811fee0799cc9634f536c9c5
Dimrondvv <83478759+Dimrondvv@users.noreply.github.com>
2024-05-31 19:28:17 +0200

    Add Summary scene scripts

--- MedievalDoc/Assets/Scripts/UI/SummaryUI/SummaryUI.cs
+++ MedievalDoc/Assets/Scripts/UI/SummaryUI/SummaryUI.cs
@@ -1,9 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class SummaryUI : MonoBehaviour
 {
+    [SerializeField]
+    private TMP_Text HealedPatientText;
+
+    [SerializeField]
+    private TMP_Text UpsetPatientText;
+
+    [SerializeField]
+    private TMP_Text EarnedMoneyText;
+
+    [SerializeField]
+    private TMP_Text BillText;
+
+    [SerializeField]
+    private TMP_Text DeathsText;
+
     // Start is called before the first frame update
     void Start()
     {
