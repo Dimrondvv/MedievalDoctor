35071f6b93d24cb30d6e0b62072ff9f3d59dd64d
SoVerrr <82769474+SoVerrr@users.noreply.github.com>
2024-06-06 19:13:56 +0200

    Buy furniture

---
+++ MedievalDoc/Assets/Scripts/Furniture/BuyFurniture.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class BuyFurniture : MonoBehaviour, IInteractable
+{
+    [SerializeField] int price;
+    [SerializeField] List<Behaviour> componentsToEnable;
+    [SerializeField] bool isInteractable;
+    [SerializeField] GameObject boughtModel;
+    [SerializeField] GameObject notBoughtModel;
+    [SerializeField] private float interactionTime;
+    [SerializeField] private TextMeshPro priceText;
+    bool textflag = false;
+    public float InteractionTime { get { return interactionTime;} set { } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PickupController.OnInteract.AddListener(Buy);    
+    }
+    public void Buy(GameObject obj, PickupController player)
+    {
+        if (obj != gameObject)
+            return;
+
+        foreach(var cmp in componentsToEnable)
+        {
+            cmp.enabled = true;
+        }
+        if (isInteractable)
+            gameObject.layer = 7;
+        App.Instance.GameplayCore.PlayerManager.Money -= price;
+        boughtModel.SetActive(true);
+        notBoughtModel.SetActive(false);
+        Destroy(priceText.gameObject);
+        Destroy(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(SharedOverlapBox.HighestCollider.gameObject == gameObject && !textflag)
+        {
+            priceText.gameObject.SetActive(true);
+            priceText.text = price.ToString() + "$";
+            textflag = true;
+        }
+        else if(SharedOverlapBox.HighestCollider.gameObject != gameObject && textflag)
+        {
+            priceText.gameObject.SetActive(false);
+            textflag = false;
+        }
+    }
+}
