f619e02de166ad4c7e5e4ae7a5d390fe56fdbd9a
Dimrondvv <dominik.glowinski2@gmail.com>
2024-07-06 09:45:05 +0200

    Added newspaper story

--- MedievalDoc/Assets/Scripts/NewsPaper/NewspaperNews.cs
+++ MedievalDoc/Assets/Scripts/NewsPaper/NewspaperNews.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewspaperScriptable", menuName = "ScriptableObjects/NewspaperNews", order = 1)]
 public class NewspaperNews : ScriptableObject
 {
+    [TextAreaAttribute(5,20)]
     public List<string> EventText;
     public List<int> EventDay;
 }
