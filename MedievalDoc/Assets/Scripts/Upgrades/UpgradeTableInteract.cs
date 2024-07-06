0cc8a8e0e61b7feacc5ad15807fa5caa77b1eb30
pawelek4318 <pawmrzy@gmail.com>
2024-07-06 15:06:59 +0200

    Upgrade text display
    Dodano wyświetlanie tekstu "Upgrade available" na stole jak upgrade jest dostępny.

--- MedievalDoc/Assets/Scripts/Upgrades/UpgradeTableInteract.cs
+++ MedievalDoc/Assets/Scripts/Upgrades/UpgradeTableInteract.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UpgradeTableInteract : MonoBehaviour, IInteractable
 {
+    [SerializeField] GameObject upgradeText;
     public float InteractionTime { get; set; }
     private UIManager manager;
     // Start is called before the first frame update
@@ -25,6 +27,7 @@ public class UpgradeTableInteract : MonoBehaviour, IInteractable
     {
         PickupController.OnInteract.AddListener(OnTableInteract);
         gameObject.layer = 7;
+        upgradeText.SetActive(true);
     }
 
     private void OnTableInteract(GameObject obj, PickupController pcp) 
@@ -35,5 +38,6 @@ public class UpgradeTableInteract : MonoBehaviour, IInteractable
         manager.UpgradeBoard();
         PickupController.OnInteract.RemoveListener(OnTableInteract);
         gameObject.layer = 0;
+        upgradeText.SetActive(false);
     }
 }
