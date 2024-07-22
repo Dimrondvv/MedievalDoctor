407f7b4b2784d7d2e2d3426e71b1ed525147378b
pawelek4318 <pawmrzy@gmail.com>
2024-07-22 22:20:39 +0200

    Interaction
    - pickup/putdown implemented

--- MedievalDoc/Assets/Scripts/Interaction/Interactor.cs
+++ MedievalDoc/Assets/Scripts/Interaction/Interactor.cs
@@ -8,9 +8,12 @@ public class Interactor : MonoBehaviour
     [SerializeField] private Transform _interactionPoint;
     [SerializeField] private float _interactionPointRadius = 0.5f;
     [SerializeField] private LayerMask _interactableMask;
+    [SerializeField] private LayerMask _laydownMask;
 
-    private readonly Collider[] _colliders = new Collider[3];
+    private readonly Collider[] _colliders = new Collider[10];
+    private readonly Collider[] _laydownColliders = new Collider[10];
     [SerializeField] private int _numFound;
+    [SerializeField] private int _numLaydownFound;
 
     private PlayerInputActions playerInputActions;
     private bool isInteracting;
@@ -34,6 +37,7 @@ public class Interactor : MonoBehaviour
     private void Update()
     {
         _numFound = Physics.OverlapSphereNonAlloc(_interactionPoint.position, _interactionPointRadius, _colliders, _interactableMask);
+        _numLaydownFound = Physics.OverlapSphereNonAlloc(_interactionPoint.position, _interactionPointRadius, _laydownColliders, _laydownMask);
 
         if (isInteracting && _numFound > 0)
         {
@@ -46,60 +50,94 @@ public class Interactor : MonoBehaviour
         }
     }
 
-    private IInteract GetClosestInteractable()
+    private Collider GetClosestInteractable()
     {
-        IInteract closestInteractable = null;
+        Collider closestCollider = null;
         float closestDistance = float.MaxValue;
 
         for (int i = 0; i < _numFound; i++)
         {
             float distance = Vector3.Distance(_interactionPoint.position, _colliders[i].transform.position);
-            if (distance < closestDistance)
+            if (distance < closestDistance && _colliders[i].GetComponent<IInteract>() != null)
             {
                 closestDistance = distance;
-                closestInteractable = _colliders[i].GetComponent<IInteract>();
+                closestCollider = _colliders[i];
             }
         }
 
-        return closestInteractable;
+        return closestCollider;
+    }
+
+    private Collider GetClosestLaydownPoint()
+    {
+        Collider closestCollider = null;
+        float closestDistance = float.MaxValue;
+
+        for (int i = 0; i < _numLaydownFound; i++)
+        {
+            float distance = Vector3.Distance(_interactionPoint.position, _laydownColliders[i].transform.position);
+            if (distance < closestDistance && _laydownColliders[i].GetComponent<IInteract>() == null)
+            {
+                // Check if the laydown point is already occupied by another interactable object
+                bool isOccupied = Physics.CheckSphere(_laydownColliders[i].transform.position, 0.1f, _interactableMask);
+                if (!isOccupied)
+                {
+                    closestDistance = distance;
+                    closestCollider = _laydownColliders[i];
+                }
+            }
+        }
+
+        return closestCollider;
     }
 
     private void OnInteractPressPerformed(InputAction.CallbackContext context)
     {
-        if (_numFound > 0)
+        var closestInteractable = GetClosestInteractable();
+        if (closestInteractable != null)
         {
-            var closestInteractable = GetClosestInteractable();
-            if (closestInteractable != null && !(closestInteractable is ToolInteraction))
-            {
-                closestInteractable.Interact(this);
-                isInteracting = true;
-                Debug.Log("Interaction started with " + closestInteractable);
-            }
+            closestInteractable.GetComponent<IInteract>().Interact(this);
+            isInteracting = true;
+            Debug.Log("Interaction started with " + closestInteractable.name);
         }
     }
 
     private void OnInteractPressCanceled(InputAction.CallbackContext context)
     {
         isInteracting = false;
-        if (_numFound > 0)
+        var closestInteractable = GetClosestInteractable();
+        if (closestInteractable != null)
         {
-            var closestInteractable = GetClosestInteractable();
-            if (closestInteractable != null)
-            {
-                Debug.Log("Interaction stopped with " + closestInteractable);
-            }
+            Debug.Log("Interaction stopped with " + closestInteractable.name);
         }
     }
 
     private void OnPickupPerformed(InputAction.CallbackContext context)
     {
-        if (_numFound > 0)
+        var playerController = App.Instance.GameplayCore.PlayerManager.PickupController.GetPickupController();
+
+        if (playerController.PickedItem == null)
         {
+            // Try to find the closest interactable object to pick up
             var closestInteractable = GetClosestInteractable();
             if (closestInteractable != null)
             {
-                closestInteractable.Pickup(this);
-                Debug.Log("Picked up " + closestInteractable);
+                closestInteractable.GetComponent<IInteract>().Pickup(this);
+                Debug.Log("Picked up " + closestInteractable.name);
+            }
+        }
+        else
+        {
+            // Try to find the closest valid laydown point to put down the item
+            var closestLaydownPoint = GetClosestLaydownPoint();
+            if (closestLaydownPoint != null)
+            {
+                playerController.PutDownItemAt(closestLaydownPoint.transform);
+                Debug.Log("Put down item at " + closestLaydownPoint.name);
+            }
+            else
+            {
+                Debug.LogWarning("No valid laydown point found");
             }
         }
     }
--- MedievalDoc/Assets/Scripts/Interaction/ToolInteraction.cs
+++ MedievalDoc/Assets/Scripts/Interaction/ToolInteraction.cs
@@ -1,24 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem.EnhancedTouch;
 
 public class ToolInteraction : MonoBehaviour, IInteract
 {
     public string InteractionPrompt => "Interacting";
 
-    private GameObject finger;
-    private Transform toolPickupPoint;
-    private PickupController pickupController;
+    [SerializeField] private float pickupRange;
+    private PickupController playerController;
+    private PlayerInputActions playerInputActions;
 
-    void Start()
+    private void Start()
     {
-        PickupController.OnPickup.AddListener(PickupTool);
-        PickupController.OnPutdown.AddListener(PutdownTool);
-        PlayerManager playerManager = App.Instance.GameplayCore.PlayerManager;
-        toolPickupPoint = playerManager.PickupController.GetToolPickupPoint();
-        finger = playerManager.PickupController.GetFingerObject();
-        pickupController = playerManager.PickupController.GetPickupController();
+        playerController = App.Instance.GameplayCore.PlayerManager.PickupController.GetPickupController();
     }
 
     public bool Interact(Interactor interactor)
@@ -29,38 +21,13 @@ public class ToolInteraction : MonoBehaviour, IInteract
 
     public bool Pickup(Interactor interactor)
     {
-        if (pickupController.PickedItem != null)
+        if (playerController.PickedItem == null)
         {
-            PutdownTool();
+            // Pick up the item
+            PickupController.OnPickup?.Invoke(this.gameObject, interactor.transform);
+            playerController.SetPickedItem(this.gameObject);
+            Debug.Log("Picked up tool.");
         }
-
-        PickupTool(this.gameObject, interactor.transform);
-
         return true;
-    }
-
-    private void PickupTool(GameObject pickedTool, Transform objectPoint)
-    {
-        Debug.Log("Picking up tool");
-        if (pickedTool != this.gameObject)
-        {
-            Debug.Log("Picking up tool not found");
-            return;
-        }
-
-        if (pickedTool == this.gameObject)
-        {
-
-            ItemLayDownPoint parentLayDown = transform.parent.GetComponent<ItemLayDownPoint>();
-            if (parentLayDown != null)
-                parentLayDown.checkIfOccupied = false;
-
-            pickupController.SetPickedItem(pickedTool);
-        }
-    }
-
-    private void PutdownTool()
-    {
-        Debug.Log("Putting down tool");
     }
 }
