28e586168f2bdddc4db318d385b7ae884e92218c
Dimrondvv <dominik.glowinski2@gmail.com>
2024-07-26 20:30:05 +0200

    Fixed Time

--- MedievalDoc/Assets/Scripts/Managers/TimerManager.cs
+++ MedievalDoc/Assets/Scripts/Managers/TimerManager.cs
@@ -9,13 +9,15 @@ public class TimerManager : MonoBehaviour
     private int elapsedTime;
     public int ElapsedTime { get { return elapsedTime; } set { elapsedTime = value; } }
 
-    float currCountdownValue;
-    public IEnumerator StartCountdown(float countdownValue = 600)
+    [SerializeField]
+    const int HowManySeconds = 600;
+
+    static public float currCountdownValue;
+    public IEnumerator StartCountdown(float countdownValue = HowManySeconds)
     {
         currCountdownValue = countdownValue;
         while (currCountdownValue > 0)
         {
-            Debug.Log("Countdown: " + currCountdownValue);
             yield return new WaitForSeconds(1.0f);
             currCountdownValue--;
         }
@@ -31,7 +33,7 @@ public class TimerManager : MonoBehaviour
     {
         StartCoroutine(StartCountdown());
         InvokeRepeating("OneSecondTimer", 0, 1);
-        elapsedTime = 0;
+        elapsedTime = HowManySeconds;
     }
     private void OnDestroy()
     {
@@ -41,6 +43,6 @@ public class TimerManager : MonoBehaviour
 
     void OneSecondTimer()
     {
-        elapsedTime+=1;
+        elapsedTime-=1;
     }
 }
